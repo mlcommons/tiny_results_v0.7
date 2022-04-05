81984a4f442f0e322ef9ff9153421589ce1a88a2
Csaba Kiraly <csaba.kiraly@gmail.com>
2022-04-05 15:21:46 +0200

    Squashed commit of the following:
    commit e8a9d8f494cd261c2114ab8ffc6cdc1f8a579665
    Author: temp <temp@temp>
    Date:   Fri Mar 4 10:55:15 2022 +0800
    
        update repo:mlperf-closed-andes-andescore-d25-d45-nx27v
               1. update doc (png and pdf)
               2. Modify json-files "Software Libraries"
               3. Add Quick_Start_Guide
    
    commit b4cb42746b56ec7a769c241d14b1559d0ad57236
    Author: temp <temp@temp>
    Date:   Thu Mar 3 14:45:53 2022 +0800
    
        update repo:mlperf-closed-andes-andescore-d25-d45-nx27v
                   1.Fixed for deafult path.
    
    commit 64f112f8c1430e2fe1a2b071556bb6f2879f77f9
    Author: temp <temp@temp>
    Date:   Thu Mar 3 14:43:51 2022 +0800
    
        update repo:mlperf-closed-andes-andescore-d25-d45-nx27v
                   1. Header modification for apache 2.0
    
    commit 400d318b4433b161dd9e0dc42195d4ae56deb2c7
    Author: temp <temp@temp>
    Date:   Tue Mar 1 11:00:13 2022 +0800
    
        update repo:mlperf-closed-andes-andescore-d25-d45-nx27v
    
    commit 58b6db98ab3b1542374f1924c4ef5cb065144454
    Author: temp <temp@temp>
    Date:   Fri Feb 18 17:00:48 2022 +0800
    
        Dump repo:mlperf-closed-andes-andescore-d25-d45-nx27v

---
+++ closed/Andes/code/README.md
@@ -0,0 +1,15 @@
+
+<font size=5> **Andes submission for MLCommons MLPerf Tiny benchmark** </font>
+	
+<font size=3> **Please refer to the documentation "Quick_Start_Guide_for_MLPerf-Tiny-Benchmark.pdf".** </font>
+
+
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_01.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_02.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_03.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_04.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_05.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_06.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_07.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_08.PNG)
+![for image](./1_imgs/Andes_Quick_Start_Guide_for_MLPerf-Tiny-Benchmark_09.PNG)
---
+++ closed/Andes/code/image_classification/submitter_implemented.cpp
@@ -0,0 +1,223 @@
+/*
+Copyright 2020 EEMBC and The MLPerf Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This file reflects a modified version of th_lib from EEMBC. The reporting logic
+in th_results is copied from the original in EEMBC.
+==============================================================================*/
+/// \file
+/// \brief C++ implementations of submitter_implemented.h
+
+#include "api/submitter_implemented.h"
+
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <stdarg.h>
+
+#include "api/internally_implemented.h"
+#include "tensorflow/lite/micro/kernels/micro_ops.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "util/tf_micro_model_runner.h"
+#include "util/quantization_helpers.h"
+#include "tensorflow/lite/micro/examples/hello_world/constants.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/micro/examples/hello_world/hello_world_model_data.h"
+#include "./ic/ic_inputs.h"
+#include "./ic/ic_model_quant_data.h"
+#include "tensorflow/lite/micro/tools/make/targets/andesevb/ae350/bsp/perf_counter.h"
+#include "tensorflow/lite/micro/tools/make/targets/andesevb/ae350/bsp/ae350/ae350.h"
+
+
+#include <stdio.h>
+extern "C" {
+#include "uart.h"
+}
+
+volatile unsigned char gstr[0x100] __attribute__((aligned(4))) = {0};
+volatile unsigned char gstr1[0x100] __attribute__((aligned(4))) = {0};
+#define printf(arg...) do { sprintf((char *)gstr, ##arg); uart_puts((char *)gstr); } while (0)
+
+#define timestamp_in_usec ((unsigned long)(((unsigned long long )rdmcycle() * (unsigned long long) 1000000) / (unsigned long long) CPUFREQ))
+
+
+tflite::MicroModelRunner<int8_t, int8_t, 7> *runner;
+
+constexpr int kTensorArenaSize = 100 * 1024;
+uint8_t tensor_arena_local[kTensorArenaSize];
+
+// Implement this method to prepare for inference and preprocess inputs.
+void th_load_tensor() {
+  uint8_t input_quantized[kIcInputSize];
+  int8_t input_asint[kIcInputSize];
+  size_t bytes;
+  
+  bytes = ee_get_buffer(reinterpret_cast<uint8_t *>(input_quantized),
+                         kIcInputSize * sizeof(uint8_t));
+  
+  if (bytes / sizeof(uint8_t) != kIcInputSize) {
+    th_printf("Input db has %d elemented, expected %d\n", bytes / sizeof(uint8_t),
+              kIcInputSize);
+    return;
+  }
+
+  uint16_t i = 0;
+  for(i=0; i<kIcInputSize;i++)
+  {
+      if(input_quantized[i]<=127)
+        input_asint[i] = ((int8_t)input_quantized[i]) - 128;
+      else
+        input_asint[i] = (int8_t)(input_quantized[i] - 128);
+  }
+
+  runner->SetInput(input_asint);
+}
+
+
+// Add to this method to return real inference results.
+void th_results() {
+  const int nresults = 10;
+  /**
+   * The results need to be printed back in exactly this format; if easier
+   * to just modify this loop than copy to results[] above, do that.
+   */
+  th_printf("m-results-[");
+
+  for (size_t i = 0; i < kCategoryCount; i++) {
+    float converted =
+        DequantizeInt8ToFloat(runner->GetOutput()[i], runner->output_scale(),
+                              runner->output_zero_point());
+
+	// Some platforms don't implement floating point formatting.
+	th_printf("%0.3f", (double) converted);
+    if (i < (nresults - 1)) {
+      th_printf(",");
+    }
+  }
+  th_printf("]\r\n");
+}
+
+// Implement this method with the logic to perform one inference cycle.
+void th_infer() { runner->Invoke(); }
+
+/// \brief optional API.
+void th_final_initialize(void) {
+  
+  static tflite::MicroMutableOpResolver<7> resolver;
+
+  resolver.AddAdd();
+  resolver.AddFullyConnected();
+  resolver.AddConv2D();
+  resolver.AddDepthwiseConv2D();
+  resolver.AddReshape();
+  resolver.AddSoftmax();
+  resolver.AddAveragePool2D();
+  //static tflite::AllOpsResolver resolver;
+
+  static tflite::MicroModelRunner<int8_t, int8_t, 7> model_runner(
+         pretrainedResnet_quant_tflite, resolver, tensor_arena_local, kTensorArenaSize);
+  runner = &model_runner;
+}
+
+void th_pre() {}
+void th_post() {}
+
+void th_command_ready(char volatile *p_command) {
+  p_command = p_command;
+  ee_serial_command_parser_callback((char *)p_command);
+}
+
+// th_libc implementations.
+int th_strncmp(const char *str1, const char *str2, size_t n) {
+  return strncmp(str1, str2, n);
+}
+
+char *th_strncpy(char *dest, const char *src, size_t n) {
+  return strncpy(dest, src, n);
+}
+
+size_t th_strnlen(const char *sstr, size_t maxlen) {
+  return strlen(sstr);
+}
+
+char *th_strcat(char *dest, const char *src) { return strcat(dest, src); }
+
+char *th_strtok(char *str1, const char *sep) { return strtok(str1, sep); }
+
+int th_atoi(const char *str) { return atoi(str); }
+
+void *th_memset(void *b, int c, size_t len) { return memset(b, c, len); }
+
+void *th_memcpy(void *dst, const void *src, size_t n) {
+  return memcpy(dst, src, n);
+}
+
+
+int th_vprintf(const char *format, va_list ap) {
+	vsprintf((char *)gstr1, format, ap);
+	printf((char *)gstr1);
+	return 0; 
+}
+
+void th_printf(const char *p_fmt, ...) {
+  va_list args;
+  va_start(args, p_fmt);
+  (void)th_vprintf(p_fmt, args); /* ignore return */
+  va_end(args);
+}
+
+char th_getchar() { return (char) uart_getc(); }
+
+void th_serialport_initialize(void) {
+# if EE_CFG_ENERGY_MODE==1
+  //pc.baud(9600);
+  uart_init(9600);
+# else
+  //pc.baud(115200);
+  uart_init(115200);
+# endif
+
+  tflite::InitializeTarget();
+}
+
+void th_timestamp(void) {
+# if EE_CFG_ENERGY_MODE==1
+  //timestampPin = 0;
+  //for (int i=0; i<100'000; ++i) {
+  //  asm("nop");
+  //}
+  //timestampPin = 1;
+# else
+  unsigned long microSeconds = 0ul;
+  /* USER CODE 2 BEGIN */
+  //microSeconds = us_ticker_read();
+  microSeconds = timestamp_in_usec;
+  /* USER CODE 2 END */
+  /* This message must NOT be changed. */
+  th_printf(EE_MSG_TIMESTAMP, microSeconds);
+# endif  
+}
+
+void th_timestamp_initialize(void) {
+  /* USER CODE 1 BEGIN */
+  // Setting up BOTH perf and energy here
+  /* USER CODE 1 END */
+  /* This message must NOT be changed. */
+  th_printf(EE_MSG_TIMESTAMP_MODE);
+  /* Always call the timestamp on initialize so that the open-drain output
+     is set to "1" (so that we catch a falling edge) */
+  th_timestamp();
+}
